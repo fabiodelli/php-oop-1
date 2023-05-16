9adea5a91e4aba935c5df4df5bc567b395afedd6
fabiodelli <ntqdel@hotmail.com>
2023-05-17 00:33:03 +0200

    add implode

--- db.php
+++ db.php
@@ -2,8 +2,8 @@
 require_once 'models/Movie.php';
 
 $movies = [
-    new Movie("Il Signore degli Anelli", "Fantasy", "196"),
-    new Movie("Inception", "Thriller", "131"),
+    new Movie("Il Signore degli Anelli", ["Fantasy"], "196"),
+    new Movie("Inception", ["Thriller"], "131"),
     new Movie("Titanic", ["Romantico", "Drammatico"], "110")
 
 ];
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -27,8 +32,8 @@
                         <?php echo $movie->getTitle(); ?><br>
 
                         <strong>Genere:</strong>
-                        <?php echo $movie->getGenere(); ?><br>
-
+                        <?php echo implode(", ", $movie->getGenere()); ?><br>
+                        
                         <strong>Durata:</strong>
                         <?php echo $movie->getDuration(); ?> <span> minuti</span>
                     </li>
@@ -38,7 +43,6 @@
 
     </div>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-    <script src="./assets/js/main.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossorigin="anonymous"></script>
